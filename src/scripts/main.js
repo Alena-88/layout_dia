a7f24cb66ba2db36ac7bc06459225dc86c61504a
Alena <alenadorohova95@gmail.com>
2026-06-19 16:38:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const images = [
+  './src/images/photo-header.png',
+  './src/images/slider/header-slider-laptop.png',
+  './src/images/slider/header-slider-headphone.png',
+];
+
+let currentIndex = 0;
+
+const image = document.getElementById('sliderImage');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  image.src = images[currentIndex];
+});
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  image.src = images[currentIndex];
+});
